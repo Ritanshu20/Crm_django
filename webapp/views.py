9eb8eee3ab17570fb2640686cdd141a83d406de3
Ritanshu Juneja <ritanshujuneja@gmail.com>
2025-06-21 17:44:30 +0530

    alomst done 3:00

--- webapp/views.py
+++ webapp/views.py
@@ -1,8 +1,14 @@
 from django.shortcuts import render, redirect
-from .forms import CreateUserForm, LoginForm
+from .forms import CreateUserForm, LoginForm , CreateRecordForm , UpdateRecordForm 
 
 from django.contrib.auth.models import auth
 from django.contrib.auth import authenticate
+from django.contrib.auth.decorators import login_required
+
+from .models import Record
+
+
+
 def home(request):
     return render(request, 'webapp/index.html')  
 
@@ -17,7 +23,7 @@ def register(request):
         if form.is_valid():
             form.save()
 
-            #return redirect()
+            return redirect('my-login')
 
 
 
@@ -32,8 +38,8 @@ def my_login(request):
         
         form  =  LoginForm(request,data=request.POST)
         if form.is_valid():
-            username = request.POSt.get('username')
-            password = request.POSt.get('password')
+            username = request.POST.get('username')
+            password = request.POST.get('password')
 
             user =  authenticate(request,  username=username, password=password)
 
@@ -41,7 +47,51 @@ def my_login(request):
             if user  is not None:
                 auth.login(request, user)
 
-                #return redirect('')
+                return redirect('dashboard')
     context = {'form': form}
 
     return render(request , 'webapp/my-login.html', context=context) 
+
+
+@login_required(login_url='my-login')
+def dashboard(request):
+
+    my_records =  Record.objects.all()
+    context =  {'records': my_records}
+
+
+
+    return render(request, 'webapp/dashboard.html', context=context )
+
+@login_required(login_url='my-login')
+def create_record(request):
+
+    form = CreateRecordForm()
+
+    if request.method == "POST":
+
+        form = CreateRecordForm(request.POST)
+
+        if form.is_valid():
+
+            form.save()
+
+            #messages.success(request, "Your record was created!")
+
+            return redirect("dashboard")
+
+    context = {'form': form}
+
+    return render(request, 'webapp/create-record.html', context=context)
+
+
+
+
+
+
+def user_logout(request):
+     auth.logout(request)
+
+     return redirect('my-login')
+
+
